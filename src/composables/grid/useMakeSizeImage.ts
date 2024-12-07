f5369daf07c6cb44004d6057d1987487e34ab4b4
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-12-07 18:48:19 +0300

    fix make size

--- src/composables/grid/useMakeSizeImage.ts
+++ src/composables/grid/useMakeSizeImage.ts
@@ -1,20 +1,14 @@
 import type {Node} from "../types/grid.type"
 
 export const useMakeSizeImage = (w: Node) => {
-  if (w.w === 1 || w.h === 1) {
-    return w.image.xs
-  }
+  const size = Math.max(w.w, w.h);
+  const sizes = {
+    1: 'xs',
+    2: 'sm',
+    3: 'md',
+    4: 'original'
+  };
 
-  if (w.w === 2 || w.h === 2) {
-    return w.image.sm
-  }
-
-  if (w.w === 3 || w.h === 3) {
-    return w.image.md
-  }
-
-  if (w.w === 4 || w.h === 4) {
-    return w.image.original
-  }
+  return w.image[sizes[size]];
 }
 
